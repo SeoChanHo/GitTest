cffab547f53b5fe49f913ba79506ec6f29248dc3
SeoChanHo <102764542+SeoChanHo@users.noreply.github.com>
2022-12-02 10:04:31 +0900

    test1

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by 서찬호 on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
